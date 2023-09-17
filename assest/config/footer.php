f48d214ec1b300ea0477cbde60d653839fdbde13
Erwin Isla <erwin.2785@gmail.com>
2023-09-17 12:25:23 -0300

    Carga Inicial

---
+++ assest/config/footer.php
@@ -0,0 +1,13 @@
+<footer class="main-footer">
+    <div class="pull-right d-none d-sm-inline-block">
+        <ul class="nav nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+		  <li class="nav-item">
+			<a class="nav-link" href="javascript:void(0)">Ultima Actualización</a>
+		  </li>
+		  <li class="nav-item">
+			<a class="nav-link" href="#">Version 3.1 02-2022</a>
+		  </li>
+		</ul>
+    </div>
+	  &copy; 2021 <a href="#">Desarrollado: Volcan Foods Ltda</a>. Todos los derechos reservados.
+  </footer>
